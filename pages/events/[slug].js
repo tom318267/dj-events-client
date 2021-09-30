0eef3e2861f8c042db167225166344f5dc3ecaa8
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-09-30 17:49:04 -0400

    test

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -20,7 +20,7 @@ const EventPage = ({ event }) => {
         {event.image && (
           <div className="my-4">
             <Image
-              src={event.image.url}
+              src={event.image.thumbnail.url}
               width={960}
               height={600}
               objectFit="contain"
