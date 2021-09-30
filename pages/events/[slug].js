7e574fa95455dcadbff1251101bdd08a8c50316d
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-09-30 18:01:23 -0400

    test

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -51,38 +51,38 @@ const EventPage = ({ event }) => {
 
 export default EventPage;
 
-// export async function getStaticPaths() {
-//   const res = await fetch(`${API_URL}/events`);
-//   const events = await res.json();
-
-//   const paths = events.map((event) => ({
-//     params: { slug: event.slug },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: true,
-//   };
-// }
-
-// export async function getStaticProps({ params: { slug } }) {
-//   const res = await fetch(`${API_URL}/events?slug=${slug}`);
-//   const events = await res.json();
-//   return {
-//     props: {
-//       event: events[0],
-//       revalidate: 1,
-//     },
-//   };
-// }
-
-export async function getServerSideProps({ query: { slug } }) {
-  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/events`);
   const events = await res.json();
 
+  const paths = events.map((event) => ({
+    params: { slug: event.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const events = await res.json();
   return {
     props: {
       event: events[0],
+      revalidate: 1,
     },
   };
 }
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: {
+//       event: events[0],
+//     },
+//   };
+// }
